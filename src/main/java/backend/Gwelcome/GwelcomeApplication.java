58710d1dc5c9fc25cab97e61fed2259307df5ea7
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-11-23 12:23:10 +0900

    feat : initial setting

---
+++ src/main/java/backend/Gwelcome/GwelcomeApplication.java
@@ -0,0 +1,13 @@
+package backend.Gwelcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwelcomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/Gwelcome/GwelcomeApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.Gwelcome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwelcomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
