d4e279d93a52086769ea12b3429e8a8aeb725389
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-12-09 22:12:00 +0900

    feat : Java 8 LocalDateTime 직렬화 역직렬화 오류 해결

--- src/main/java/backend/Gwelcome/config/SpringConfig.java
+++ src/main/java/backend/Gwelcome/config/SpringConfig.java
@@ -2,6 +2,7 @@ package backend.Gwelcome.config;
 
 import backend.Gwelcome.controller.ResponseEntityByWebClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +18,9 @@ public class SpringConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 
     @Bean
